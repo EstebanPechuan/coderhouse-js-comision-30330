12969cbacf99bc606e8d61dea51b5bc22d5a5a51
EstebanPechuan <esteban.pechuan@gmail.com>
2022-05-28 21:31:34 -0300

    Loading

--- App - Clima/src/js/main.js
+++ App - Clima/src/js/main.js
@@ -1,40 +1,44 @@
 const body = document.querySelector('body')
-const cardContainer = document.querySelector('.containerCards')
-const ciudad = document.querySelector('#ciudad')
-const imagen = document.querySelector('#weatherImg')
-const temperatura = document.querySelector('#temperatura')
-const maxTemp = document.querySelector('#max')
-const minTemp = document.querySelector('#min')
-const cielo = document.querySelector('#cielo')
+const cardContainer = document.querySelector('.containerCard')
+const form = document.querySelector('#formCiudad')
+const input = document.querySelector('#city')
+
+const apiKey = "18962b97fd85aa4544014810f8b7e95f";
 
 let estadoClima
 let inputCiudad
 let inputCiudadValue
-let form = document.querySelector('#formCiudad')
-let input = document.querySelector('#city')
 
-let arrayFav = []
-let arryClimaTotal = []
-
-const buscarCiudad = (e) => {
-    e.preventDefault()
-    inputCiudad = document.querySelector('#city')
-    inputCiudadValue = document.querySelector('#city').value
-    
-    buscarClima(inputCiudadValue)
-}
+var arryClimaTotal = []
 
 form.addEventListener('submit', (e) => buscarCiudad(e))
 input.addEventListener('click', (e) => buscarCiudad(e))
 
-const apiKey = "18962b97fd85aa4544014810f8b7e95f";
+const buscarCiudad = (e) => {
+    e.preventDefault()
+    inputCiudadValue = input.value
+    
+    buscarClima(inputCiudadValue)
+}
+
 
 const buscarClima = (city) => {
-    const inputDOM = document.querySelector('#city')
-    inputDOM.value = ''
-    inputDOM.focus()
+    if (city === '') {
+        return
+    }
+
+    cardContainer.innerHTML = `
+        <div class="loader">
+            <span></span>
+        </div>
+    `
+
+    input.value = ''
+    input.focus()
 
     let apiGeo = `http://api.openweathermap.org/geo/1.0/direct?q=${city}&limit=5&appid=${apiKey}`;
+
+
     
     window.fetch(apiGeo)
         .then(lat_long => lat_long.json())
@@ -44,15 +48,12 @@ const buscarClima = (city) => {
 
             let api = `https://api.openweathermap.org/data/2.5/weather?lat=${coord_lat}&lon=${coord_long}&appid=${apiKey}`;
 
-
             window.fetch(api)
                 .then(data => data.json())
                 .then(data => {
                     estadoClima = data.weather[0].description
                     
-                    let actualWeather = {
-                         favorito: false
-                    }
+                    let actualWeather = {}
                     
                     actualWeather.image = data.weather[0].icon
                     actualWeather.ciudad = data.name
@@ -60,39 +61,82 @@ const buscarClima = (city) => {
                     actualWeather.temp = Math.round((data.main.temp - 273.15) * 100) / 100
                     actualWeather.maxTemp = Math.round((data.main.temp_max - 273.15) * 100) / 100
                     actualWeather.minTemp = Math.round((data.main.temp_min - 273.15) * 100) / 100
-                    actualWeather.cielo = data.weather[0].description
+                    actualWeather.cielo = estadoClima
+
+                    console.log(actualWeather);
 
                     arryClimaTotal.push(actualWeather)
+
+                    let cardClima = `
+                        <div class="card">
+                            <h2 id="ciudad">${actualWeather.ciudad} - ${actualWeather.pais}</h2>
+                
+                            <div class="img-info">
+                                <div class="img-weather">
+                                    <img id="weatherImg"
+                                        src="http://openweathermap.org/img/wn/${actualWeather.image}@2x.png"
+                                        alt=""
+                                    >
+                                </div>
+                            
+                                <div class="info">
+                                    <p><span id="temperatura">${actualWeather.temp}</span> °C</p>
+                        
+                                    <p id="cielo">${actualWeather.cielo}</p>
+                                </div>
+                            </div>
+                        
+                            <div class="minmax">
+                                <div class="maxTemp">
+                                    Temp. máxima: <span id="max">${actualWeather.maxTemp}</span> °C
+                                </div>
+                                
+                                <div class="minTemp">
+                                    Temp. mínima: <span id="min">${actualWeather.minTemp}</span> °C
+                                </div>
+                            </div>
+                        </div>
+                    `
+    
+                    cardContainer.innerHTML = cardClima
+
+                    window.localStorage.setItem('lastWeather', JSON.stringify(arryClimaTotal))
                     
-                    imagen.src = `http://openweathermap.org/img/wn/${actualWeather.image}@2x.png`
-                    ciudad.innerText = `${actualWeather.ciudad} - ${actualWeather.pais}`
-                    temperatura.innerText = actualWeather.temp
-                    maxTemp.innerText = actualWeather.maxTemp
-                    minTemp.innerText = actualWeather.minTemp
-                    cielo.innerText = actualWeather.cielo
-
-                    window.localStorage.setItem('lastWeather', JSON.stringify(arryClimaTotal[arryClimaTotal.length-1]))
-
-                    const changeBackground = () => {
-                        if(estadoClima === 'overcast clouds') {
-                            body.classList.add('overcast-clouds')
-                        } else if (estadoClima === 'clear sky') {
-                            body.classList.add('clear-sky')
-                        } else if (estadoClima === 'scattered clouds') {
-                            body.classList.add('clouds')
-                        }  else if (estadoClima === 'few clouds' || estadoClima === 'broken clouds') {
-                            body.classList.add('few-clouds')
-                        } else {
-                            body.setAttribute('class', '')
-                        }
-                    }
-
                     changeBackground()
                 })  
             })
         }
 
-const getLocal = JSON.parse(window.localStorage.getItem('lastWeather'))
+const changeBackground = () => {
+    if(estadoClima === 'overcast clouds') {
+        body.classList.add('overcast-clouds')
+    } else if (estadoClima === 'clear sky') {
+        body.classList.add('clear-sky')
+    } else if (estadoClima === 'scattered clouds') {
+        body.classList.add('clouds')
+    }  else if (estadoClima === 'few clouds' || estadoClima === 'broken clouds') {
+        body.classList.add('few-clouds')
+    } else {
+        body.classList.add('clear-sky')
+    }
+}
+
+
+var getLocal = JSON.parse(window.localStorage.getItem('lastWeather'))
+
 if ( getLocal ) {
-    buscarClima(getLocal.ciudad)
+    arryClimaTotal = getLocal
+    let ultimaCiudadGuardada = arryClimaTotal[arryClimaTotal.length-1].ciudad
+    console.log(ultimaCiudadGuardada);
+    buscarClima(ultimaCiudadGuardada)
+} else {
+    input.value = ''
+    input.focus()
+    cardContainer.innerHTML = `
+        <div class="card">
+            <div class="ingresarCiudad">
+                <h3>Ingrese una ciudad para comenzar</h3>
+            </div>
+        </div>
+    `
 }
